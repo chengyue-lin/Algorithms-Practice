bf2a0eda86b9eee6cb1bdc75f2dcc43d5122e307
sachin1092 <me@sachinshinde.com>
2015-03-08 20:07:42 +0530

    xml1

---
+++ xml1.py
@@ -0,0 +1,30 @@
+import xml.etree.ElementTree as etree
+from sys import argv
+
+def main(input_file):
+	with open(input_file, 'r') as f:
+		cases = f.readlines()
+	# n = input()
+	xml = ''
+	# for __ in xrange(n):
+	# 	xml += raw_input()
+	# 	print xml
+	# print "xml is:\n", xml
+	
+	for line in cases[1:]:
+		xml += line
+
+	print xml	 
+	
+	tree = etree.ElementTree(etree.fromstring(xml))
+	print tree
+	score = 0
+	for child in tree.iter():
+		score += len(child.items())
+	print score
+
+if __name__ == '__main__':
+	if len(argv) == 2:
+		main(argv[1])
+	else:
+		main('input')
