c94d571e6a6476eb61e899fa6a02c286a52f2c8f
sachin1092 <me@sachinshinde.com>
2015-03-11 20:44:51 +0530

    set difference with sort completed

--- symmetric_difference.py
+++ symmetric_difference.py
@@ -5,8 +5,8 @@ def main(input_file):
 		cases = f.readlines()
 		set1 = set(map(int, cases[1].split()))
 		set2 = set(map(int, cases[3].split()))
-		print set1, set2
-		for val in set1.union(set2).difference(set1.intersection(set2))):
+		final_set = set1.union(set2).difference(set1.intersection(set2))
+		for val in sorted(final_set):
 			print val
 
 
