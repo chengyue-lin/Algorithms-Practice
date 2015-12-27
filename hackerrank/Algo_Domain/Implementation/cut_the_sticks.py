338e83de4ac2a17574bf046ac395dfd9e9db3d9e
sachin1092 <me@sachinshinde.com>
2015-12-26 22:14:36 -0800

    improved complexity

--- hackerrank/Algo_Domain/Implementation/cut_the_sticks.py
+++ hackerrank/Algo_Domain/Implementation/cut_the_sticks.py
@@ -1,6 +1,8 @@
 n = int(raw_input())
-arr = map(int, raw_input().strip().split())
-while len(arr):
-    item = min(arr)
-    print len(arr)
-    arr = [x for x in arr if item != x]
+arr = sorted(map(int, raw_input().strip().split()))
+prev_item = arr[0]
+print len(arr)
+for index, item in enumerate(arr):
+    if item != prev_item:
+        print len(arr[index:])
+        prev_item = item
