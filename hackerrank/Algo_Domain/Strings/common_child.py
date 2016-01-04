c13a58a0ba297487e1db3c2ec80385db1d220699
sachin1092 <me@sachinshinde.com>
2016-01-04 13:21:40 -0800

    changed dict to list in common child

--- hackerrank/Algo_Domain/Strings/common_child.py
+++ hackerrank/Algo_Domain/Strings/common_child.py
@@ -1,18 +1,16 @@
 s1 = raw_input().strip()
 s2 = raw_input().strip()
 
-x = {}
+x = [[0 for __ in xrange(len(s2) + 1)] for __ in xrange(len(s1) + 1)]
 max_ = 0
 
-for i in xrange(len(s1) + 1):
-	for j in xrange(len(s2) + 1):
-		if i == 0 or j == 0:
-			x[i, j] = 0
-		elif s1[i - 1] == s2[j - 1]:
-			x[i, j] = x[i - 1, j - 1] + 1
-		else:
-			x[i, j] = max(x[i - 1, j], x[i, j - 1])
-		if max_ < x[i, j]:
-			max_ = x[i, j]
+for i in xrange(len(s1)):
+    for j in xrange(len(s2)):
+        if s1[i] == s2[j]:
+            x[i + 1][j + 1] = x[i][j] + 1
+        else:
+            x[i + 1][j + 1] = max(x[i, j + 1], x[i + 1, j])
+        if max_ < x[i + 1][j + 1]:
+            max_ = x[i + 1][j + 1]
 
 print max_
