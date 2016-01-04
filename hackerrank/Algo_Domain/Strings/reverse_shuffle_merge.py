2b101afa850e70ca343403843a2571710e75b893
sachin1092 <me@sachinshinde.com>
2016-01-03 17:26:14 -0800

    tring another approach

--- hackerrank/Algo_Domain/Strings/reverse_shuffle_merge.py
+++ hackerrank/Algo_Domain/Strings/reverse_shuffle_merge.py
@@ -5,15 +5,30 @@ letters = list(set(list(s)))
 letters.sort()
 counter = {x: y/2 for x, y in Counter(s).iteritems()}
 A = ''
+smallest_yet = chr(123)
 for i in xrange(len(s) - 1, -1, -1):
     x = s[i]
     c = s[i-1::-1].count(x)
+    print "smallest_yet", smallest_yet
+    if smallest_yet > x and x in letters:
+        smallest_yet = x
+    print "\n"
+    print s[i-1::-1]
+    print x
+    print letters
+    print "c", c
+    print "counter[",x,"]", counter[x]
     if c < counter[x]:
-        A += x
+        A += smallest_yet
+        smallest_yet = chr(123)
         counter[x] -= 1
-    elif x == letters[0]:
-        A += x
-        counter[x] -= 1
-        if counter[x] == 0:
-            letters.pop(0)
+    elif len(letters) and x == letters[0]:
+        if counter[x] > 0:
+            A += smallest_yet
+            smallest_yet = chr(123)
+            counter[x] -= 1
+        print "counter after modification", counter[x]
+    if counter[x] == 0 and x in letters:
+        letters.remove(x)
+    print "A is", A
 print A
