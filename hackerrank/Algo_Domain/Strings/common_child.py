b1931ea97836314c4b3b0f9a622e5775c4d8844a
sachin1092 <me@sachinshinde.com>
2016-01-05 23:02:35 -0800

    best one of all

--- hackerrank/Algo_Domain/Strings/common_child.py
+++ hackerrank/Algo_Domain/Strings/common_child.py
@@ -1,15 +1,24 @@
-from __future__ import division
 from sys import stdin
-from collections import defaultdict
 
 def lcs(s1, s2):
-    prev = defaultdict(int)
-    for i in range(len(s1)):
-        cur = defaultdict(int)
-        for j in range(len(s2)):
-            cur[j] = prev[j - 1] + 1 if s1[i] == s2[j] else max(cur[j - 1], prev[j])
-        prev = cur
-    return prev[len(s2)-1]
+    dp_current = []
+    dp_prev = []
+    n, m = len(s1), len(s2)
+    for __ in xrange(n+1):
+        dp_current.append(0)
+        dp_prev.append(0)
+    for i in xrange(n + 1):
+        for j in xrange(m + 1):
+            if i == 0 or j == 0:
+                dp_current[j] = 0
+            elif s1[i - 1] == s2[j - 1]:
+                dp_current[j] = dp_prev[j - 1] + 1
+            else:
+                dp_current[j] = max(dp_current[j-1], dp_prev[j])
+
+        for j in xrange(n):
+            dp_prev[j] = dp_current[j]
+    return dp_current[-1]
                 
 def main():
     s, t = stdin.next().strip(), stdin.next().strip()
