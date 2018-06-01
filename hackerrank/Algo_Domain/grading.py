95cf97044becdb0d41e508d65cd749980d910b21
Sachin Shinde <me@sachinshinde.com>
2018-05-31 22:46:51 -0500

    grading problem

---
+++ hackerrank/Algo_Domain/grading.py
@@ -0,0 +1,39 @@
+# Q. https://www.hackerrank.com/challenges/grading/problem
+
+#!/bin/python
+
+from __future__ import print_function
+
+import os
+import sys
+
+#
+# Complete the gradingStudents function below.
+#
+def gradingStudents(grades):
+    result = []
+    for grade in grades:
+        if (5 - (grade % 5) < 3 and grade >= 38):
+            result.append((grade / 5) * 5 + 5)
+        else:
+            result.append(grade)
+    return result
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(raw_input())
+
+    grades = []
+
+    for _ in xrange(n):
+        grades_item = int(raw_input())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    f.write('\n'.join(map(str, result)))
+    f.write('\n')
+
+    f.close()
+
